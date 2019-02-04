e086c63dfc3e81542d1bc1681363338826e5abd8
Eujine <sponsor85@mail.ru>
2019-02-04 22:31:35 +0200

    init commit

---
+++ SimpleMultipliers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMultipliers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRed = 3;
+            int maxBull = 5;
+            int redCounter = maxRed;
+            int bullCounter = maxBull;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if(redCounter == 0)
+                {
+                    redCounter = maxRed;
+
+                    if (bullCounter == 0)
+                    {
+                        bullCounter = maxBull;
+                        Console.WriteLine(string.Format(" i = {0} RedBull ", i));
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format(" i = {0} Red ", i));
+                    }
+
+                }
+                else
+                {
+                    if(bullCounter == 0)
+                    {
+                        bullCounter = maxBull;
+                        Console.WriteLine(string.Format(" i = {0} Bull ", i));
+                    }
+                    else
+                    {
+                        Console.WriteLine(" i = " + i.ToString());
+                    }
+                }
+
+                bullCounter--;
+                redCounter--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleMultipliers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SimpleMultipliers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SimpleMultipliers")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b4711d01-afed-44c9-b4de-665d40ffec1e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
